14800299db0e87a2bae6734809c7e143ef3d21e2
Seyeon Gwak <ksyk1205@naver.com>
2024-06-30 16:42:24 +0900

    Refactor : 레코드로 변경

--- src/main/java/com/moviereview/application/dto/MovieCreateResponse.java
+++ src/main/java/com/moviereview/application/dto/MovieCreateResponse.java
@@ -1,12 +1,10 @@
 package com.moviereview.application.dto;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class MovieCreateResponse {
-  String id;
-
+public record MovieCreateResponse(
+    String id
+) {
   @Builder
   public MovieCreateResponse(String id) {
     this.id = id;
