65a475e63d7664e8f899d5c05223bf89dff3ccdc
Seyeon Gwak <ksyk1205@naver.com>
2024-06-30 14:55:30 +0900

    Feature: Document 생성 및 이넘 타입 생성

---
+++ src/main/java/com/moviereview/domain/movie/model/Genre.java
@@ -0,0 +1,25 @@
+package com.moviereview.domain.movie.model;
+
+public enum Genre {
+  ACTION,
+  DRAMA,
+  COMEDY,
+  HORROR,
+  ROMANCE,
+  SCIENCE_FICTION,
+  THRILLER,
+  FANTASY,
+  ANIMATION,
+  DOCUMENTARY,
+  MUSICAL,
+  ADVENTURE,
+  CRIME,
+  WESTERN,
+  WAR,
+  HISTORY,
+  BIOGRAPHY,
+  SPORT,
+  FAMILY,
+  MUSIC,
+  OTHER
+}
