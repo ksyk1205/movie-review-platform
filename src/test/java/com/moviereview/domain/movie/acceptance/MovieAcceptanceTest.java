287b1c12112f15434495a23af5be47f4356dd5bb
ksyk1205 <ksyk1205@naver.com>
2024-07-02 22:20:33 +0900

    Test: 테스트 코드 리팩토링 및 리스트 조회 인수 테스트 추가

--- src/test/java/com/moviereview/domain/movie/acceptance/MovieAcceptanceTest.java
+++ src/test/java/com/moviereview/domain/movie/acceptance/MovieAcceptanceTest.java
@@ -1,14 +1,22 @@
 package com.moviereview.domain.movie.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.moviereview.common.annotation.AcceptanceTest;
+import com.moviereview.domain.movie.model.Movie;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -29,21 +37,32 @@ class MovieAcceptanceTest {
   @DisplayName("영화를 등록할 수 있다.")
   void 영화_등록_테스트() {
     //given
-    Map<String, Object> params = new HashMap<>();
-    params.put("title", "원더랜드");
-    params.put("director", "김태용");
-    params.put("release_date", "2023-06-30");
-    params.put("actors", List.of("탕웨이", "수지", "박보검"));
-    params.put("genre", "DRAMA");
 
-    // when
-    RestAssured.given().log().all()
-        .contentType(MediaType.APPLICATION_JSON_VALUE)
-        .body(params)
-        .when().post("/v1/movie")
-        // then
-        .then().log().all()
-        .statusCode(HttpStatus.OK.value()).extract();
+    //when
+    ExtractableResponse<Response> 영화_생성_response = MovieSteps.영화_생성_요청("원더랜드", "김태용",
+        LocalDate.of(2024, 06, 30), List.of("탕웨이", "수지", "박보검"), "DRAMA");
+
+    //then
+    String id = 영화_생성_response.jsonPath().getString("id");
+    assertThat(id).isNotBlank();
   }
 
+  @Test
+  @DisplayName("영화 리스트를 조회할 수 있다.")
+  void 영화_리스트_조회() {
+    //given
+    ExtractableResponse<Response> 영화_생성_response = MovieSteps.영화_생성_요청("원더랜드", "김태용",
+        LocalDate.of(2024, 06, 30), List.of("탕웨이", "수지", "박보검"), "DRAMA");
+
+    // when
+    ExtractableResponse<Response> response = RestAssured.given().log().all()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when().get("/v1/movie")
+
+    // then
+        .then().log().all()
+        .statusCode(HttpStatus.OK.value()).extract();
+
+   assertThat(response.jsonPath().getList("title").get(0)).isEqualTo("원더랜드");
+  }
 }
---
+++ src/test/java/com/moviereview/domain/movie/acceptance/MovieSteps.java
@@ -0,0 +1,33 @@
+package com.moviereview.domain.movie.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class MovieSteps {
+
+  public static ExtractableResponse<Response> 영화_생성_요청(String title, String director,
+      LocalDate releaseDate, List<String> actors, String genre) {
+
+    Map<String, Object> params = new HashMap<>();
+    params.put("title", title);
+    params.put("director", director);
+    params.put("release_date", releaseDate);
+    params.put("actors", actors);
+    params.put("genre", genre);
+
+    return RestAssured.given().log().all()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(params)
+        .when().post("/v1/movie")
+        .then().log().all()
+        .statusCode(HttpStatus.OK.value()).extract();
+  }
+
+}
